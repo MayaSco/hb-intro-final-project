ff6ffae5f110fb50d7b84be2e38d55ac08f83327
MayaSco <mayaandtyson@gmail.com>
2015-10-03 16:22:24 -0700

    addd final project.py

---
+++ final_project.py
@@ -0,0 +1,43 @@
+#War vs. Computer
+#Create a dictionary assigning values for cards in order
+#End of game is winner reaches 10 points or types exit
+goal = 10
+dealersPoints = 0
+myPoints = 0
+cards = {"Ace":14,"King":13, "Queen":12, "Jack":11, "10":10, "9":9, "8":8, "7":7, "6":6, "5":5, "4":4, "3":3, "2":2}
+#Can have it ask name
+#Ready to play?
+#Need pseudo random generator here
+dealerscard = cards [randomkey]
+print "The dealer's card is: " cards[randomkey()]
+mycard=cards[randomkey]
+print "Your card is: ", cards[randomkey()]
+def compare (dealersCard,myCard)0
+	if dealerscard>mycard:
+			dealersPoints+=1
+	if dealerscard<mycard:
+			myPoints+=1
+	if dealerscard == mycard:
+		print "Go to War"
+			War():
+def Checkwinner():
+	CheckWinner (dealersPoints,myPoints):
+	# if war occurs
+def war():
+	for i=0;i<4;i+=1(dealer)
+	print "shuffling"
+	#countdown - 3 2 1 iteration
+	dealerBattlecard = random
+	userBattlecard = random
+		if myBattlecard>dealerBattlecard
+		print "You win this battle"
+	else:
+		print "Try again!"
+
+#optional betting component - start with $100 then bet in increments of $5
+#tally high score after 10 points or else game over if busts out of money
+#beyond optional: keep track of reigning high score 
+
+
+
+
