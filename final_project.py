35c44e9d0d3966954eed772def0bfa3cc232ef85
MayaSco <mayaandtyson@gmail.com>
2015-10-11 15:54:59 -0700

    bettingFunction branch

--- final_project.py
+++ final_project.py
@@ -1,54 +1,146 @@
 #War vs. Computer
 #Create a dictionary assigning values for cards in order
-#End of game is winner reaches 10 points or types exit
+#End of game is winner reaches 10 points or myCash ==0 or player types a key to exit
+#Can have it ask name, how much to bet?
+#Need pseudo random generator here
 import random
 goal = 10
 dealersPoints = 0
 myPoints = 0
-def CheckWinner (dealersPoints,myPoints):
-	if dealersPoints == 10:
-		print "Dealer wins, better luck next time!"
-	elif myPoints == 10:
-		print " You are the big winner!"
+name = raw_input ("What is your name? ")
+print "Hello,",name, "let's play war!"
+myCash = 100 
+# keep tally of myCash and game is over if myCash<0
+myBet = int(raw_input ("You have $100 to start, minimum bet is $5. Place your bet in any increment of 5. "))
+# if myBet>myCash then print "Sorry, you don't have enough money, place another bet?
+# not sure what I was using this for
+# def ==(num1, num2):
+# 	if num1 == num2:
+# 		return True
+# 	else:
+# 		return False
+def isdivisibleby5(number):
+	return number %5==0
+def IsthereaWinner (dealersPoints,myPoints):
+	return dealersPoints == 10 or myPoints ==10
+def dealerWin (dealersCard,myCard,dealersPoints,myPoints,myCash,myBet):
+	if dealersCard>myCard:
+		print "Dealer wins this point, sorry!"
+	dealersPoints+=1
+	myCash-=myBet
+	return myCash
+def userWin(dealersCard,myCard,dealersPoints,myPoints,myCash,myBet):
+	if dealersCard<myCard:
+		print "You win this point, yay!"
+	myPoints+=1
+	myCash+=myBet
+	return myCash
+def gotoWar(dealersCard,myCard,dealersPoints,myPoints,myCash,myBet):
+	if dealersCard == myCard:
+		myBetWar = int(raw_input ("If you are ready, double your bet!"))
+	myBet+=myBet
+	return myBet
+# print "Dealer wins, better luck next time!"
+# print " You are the big winner!"
 cards = {14:"Ace",13:"King",12:"Queen",11:"Jack",10:"10", 9:"9", 8:"8", 7:"7",6:"6",5:"5",4:"4",3:"3",2:"2"}
-#switch the dictionary placement of the values above AS PER AHMAD meeting
-#Can have it ask name
-#Ready to play?
-#Need pseudo random generator here
-while (dealersPoints<11 or myPoints<11):
+while (IsthereaWinner):
 	dealerRandom=random.randrange(2,15)
 	userRandom=random.randrange(2,15)
 	dealersCard = cards [dealerRandom]
 	print "The dealer's card is: ", dealersCard
 	myCard=cards[userRandom]
 	print "Your card is: ", myCard
-	if dealersCard>myCard:
-		print "Dealer wins this point, sorry!"
-		dealersPoints+=1
-	if dealersCard<myCard:
-		print "You win this point, yay!"
-		myPoints+=1
-	if dealersCard == myCard:
-		print "Go to War"
-CheckWinner(dealersPoints,myPoints)
+	winningconditions
+	
 
-# if war occurs
-# def war():
-# 	print "shuffling"
-# 	#countdown - 3 2 1 iteration
+
+
+	if dealerRandom > userRandom:
+		dealerWin
+	elif userRandom > dealerRandom:
+		userWin
+	else:
+		GotoWar
+#
+
+	dealerWin (dealerRandom,userRandom,dealersPoints,myPoints,myCash,myBet):
+		print myCash, "is what you have left to bet."
+		myBet = int(raw_input ("Minimum bet is $5. Place your bet in any increment of 5. "))
+	elif userWin (dealerRandom,userRandom,dealersPoints,myPoints,myCash,myBet):
+		print myCash, "is what you have left to bet."
+		myBet = int(raw_input ("Minimum bet is $5. Place your bet in any increment of 5. "))
+	else: 
+		GotoWar = raw_input("Go to War?") #(Go to War? Type yes or No to surrender half your bet)
+		myBetWar = int(raw_input ("If you are ready, double your bet!"))
+		myBet+=myBet
+
+	# if dealersCard>myCard:
+	# 	print "Dealer wins this point, sorry!"
+	# 	dealersPoints+=1
+	# 	myCash-=myBet
+	# 	print myCash, "is what you have left to bet."
+	# 	myBet = int(raw_input ("Minimum bet is $5. Place your bet in any increment of 5. "))
+	# if dealersCard<myCard:
+	# 	print "You win this point, yay!"
+	# 	myPoints+=1
+	# 	myCash+=myBet
+	# 	print myCash, "is what you have left to bet."
+	# 	myBet = int(raw_input ("Minimum bet is $5. Place your bet in any increment of 5. "))
+	# if dealersCard == myCard:
+	# 	GotoWar = raw_input("Go to War?") #(Go to War? Type yes or No to surrender half your bet)
+		# myBetWar = int(raw_input ("If you are ready, double your bet!"))
+		# myBet+=myBet
+def countup(count2):
+	if(count2 == 4):
+		print "Shuffling"
+	else:
+		print count2
+		countup(count2 +1)
+countup(1)
+
+IsthereaWinner(dealersPoints,myPoints)
+# def war()
+# option: if player types no and chooses to surrender then take half of myBet and subtract it from myCash
+# countdown - 3 2 1 iteration while shuffling
+
 # 	dealerBattlecard = random
 # 	userBattlecard = random
 # 		if myBattlecard>dealerBattlecard
-
-
 # 		print "You win this battle"
 # 	else:
 # 		print "Try again!"
 
-#optional betting component - start with $100 then bet in increments of $5
+#exit the game with a key press 
 #tally high score after 10 points or else game over if busts out of money
 #beyond optional: keep track of reigning high score 
 
 
 
+def dealerWin (dealersCard,myCard,dealersPoints,myPoints,myCash,myBet):
+	if dealersCard>myCard:
+		print "Dealer wins this point, sorry!"
+	dealersPoints+=1
+	myCash-=myBet
+	return myCash
+def userWin(dealersCard,myCard,dealersPoints,myPoints,myCash,myBet):
+	if dealersCard<myCard:
+		print "You win this point, yay!"
+	myPoints+=1
+	myCash+=myBet
+	return myCash
+def gotoWar(dealersCard,myCard,dealersPoints,myPoints,myCash,myBet):
+	if dealersCard == myCard:
+		myBetWar = int(raw_input ("If you are ready, double your bet!"))
+	myBet+=myBet
+	return myBet
++++++++
+'''
+def winningconditions(dealersCard, mycard, dealrepoint, mypoint, myCash, myBet):
+	if d < U:
+		dealersPoints+=1
+	myCash-=myBet
+	return
+    elif d>u: return
+else: (gotowar condition)
 
+'''
